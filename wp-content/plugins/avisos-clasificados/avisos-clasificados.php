3743acddca504010c6da495372eb546e0efef02b
fschonffeldt <f.schonffeldtc@gmail.com>
2024-11-04 23:09:42 -0300

    fix: modificacion avisos-clasificados.php modificacion codigo

--- wp-content/plugins/avisos-clasificados/avisos-clasificados.php
+++ wp-content/plugins/avisos-clasificados/avisos-clasificados.php
@@ -9,3 +9,12 @@ require_once plugin_dir_path(__FILE__) . 'includes/posttypes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/avisos-shortcodes.php';
 
 register_activation_hook(__FILE__, 'avisos_rewrite_flush');
+
+
+// Encolar estilos desde la carpeta css en assets
+function encolar_estilos_avisos_clasificados() {
+    wp_enqueue_style('estilos-avisos', plugin_dir_url(__FILE__) . 'assets/css/styles.css');
+}
+
+// Asegurarse de usar 'wp_enqueue_scripts' correctamente
+add_action('wp_enqueue_scripts', 'encolar_estilos_avisos_clasificados');
